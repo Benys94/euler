cd7b98166c0d040d93c4b1368358790afbe3300c
Marek Jankech <marek.jankech@outlook.com>
2016-11-06 19:56:12 +0100

    Functional generator of random or connected random graphs
    If the option CONNECTED is selected, it generates a random connected graph without an assurence of existing Eulerian trail or cycle. At first, it generates a random spanning tree, then it adds other random edges.
    If the option RANDOM is selected, it generates a general random graph with the possibility of isolated vertices.
    Other options are just in development.
    
    The graph is represented by matrix (internally by one-dimensional array) and the matrix is printed to the output file or stdout, according to which option is selected.
    
    Tested on eva (FreeBSD).

---
+++ generator.c
@@ -0,0 +1,163 @@
+/**
+ *
+ * File: generator.c
+ * Description: Operations for graph generating
+ * Author: Marek Jankech <xjanke01@stud.fit.vutbr.cz>
+ *
+ **/
+
+#include "generator.h"
+
+/**
+ * Swapping function
+ */
+void swap(int* a1, int *a2 ) {
+   int aux;
+
+   aux = *a1;
+   *a1 = *a2;
+   *a2 = aux;
+}
+
+/**
+ * Random number generator, 0...n-1
+ */
+int my_random(int n) {
+   return rand() % n;
+}
+
+/**
+ * Function for rearranging elements of array
+ */
+void rearrange(int* array, int n) {
+   int i;
+
+   for (i = 0; i < n - 1; i++)
+      swap(array + i + my_random(n - i), array + i);
+}
+
+/**
+ * Function that manages generating the right graph with selected parameters and properties
+ */
+void graph_generating_manager(uint8_t *matrix, int nodes, int edges, TProperty property) {
+  switch (property) {
+    case RANDOM:
+      random_graph(matrix, nodes, edges);
+      break;
+    case CONNECTED:
+      connected_random_graph(matrix, nodes, edges);
+      break;
+    case EULER_TRAIL:
+      //euler_trail_random_graph();
+      //Should be done in near future
+      break;
+    case EULER_CYCLE:
+      //euler_cycle_random_graph();
+      //Should be done in near future
+      break;
+    default:
+      random_graph(matrix, nodes, edges);
+      break;
+  }
+}
+
+/**
+ * Generator of general random graphs without any specification and
+ * with the probablity of isolated vertices or partitions
+ */
+void random_graph(uint8_t *matrix, int nodes, int edges) {
+  int i, j, k;
+  
+  //Generate some random edges of the graph
+	for (k = 0; k < edges;) {
+		i = my_random(nodes);
+		j = my_random(nodes);
+
+		//Do not write to the diagonale and do not rewrite the edges that were already set
+    if (i != j && !matrix[i * nodes + j]) {
+			 matrix[i * nodes + j] = matrix[j * nodes + i] = 1;
+			 k++;
+		}
+  }
+}
+
+/**
+ * Generator of connected random graphs without any isolated vertices or
+ * partitions
+ */
+void connected_random_graph(uint8_t *matrix, int nodes, int edges) {
+	int *aux_array, i, j, k;
+	
+	//Allocate memory for auxilliary array
+	if((aux_array = (int *) malloc(nodes * sizeof(int))) == NULL) {
+		perror("malloc");
+		exit(1);
+	}
+  
+	//Initialize auxilliary array
+	for (i = 0; i < nodes; i++) {
+		aux_array[i] = i;
+	}
+	
+	//Rearrange elements of aux array
+	rearrange(aux_array, nodes);
+	
+	//Generate random spanning tree
+	for (i = 1; i < nodes; i++) {
+		j = my_random(i);
+		matrix[aux_array[i] * nodes + aux_array[j]] = matrix[aux_array[j] * nodes + aux_array[i]] = 1;
+  }
+	
+	//Generate remaining random edges of the graph
+	for (k = nodes - 1; k < edges;) {
+		i = my_random(nodes);
+		j = my_random(nodes);
+
+		//Do not write to the diagonale and do not rewrite the edges that were already set
+    if (i != j && !matrix[i * nodes + j]) {
+			 matrix[i * nodes + j] = matrix[j * nodes + i] = 1;
+			 k++;
+		}
+  }
+  //Free the memory for aux array
+  free(aux_array);
+}
+
+/**
+ * Function that manages printing the matrix to the output file or stdout 
+ * For debugging
+ */
+void matrix_printing_manager(int nodes, uint8_t *matrix, bool print_to_stdout) {
+	
+  if(print_to_stdout) {
+    matrix_printer(nodes, matrix, stdout);
+  }
+  else {
+    FILE *fw = fopen("matrix.txt", "w");
+    matrix_printer(nodes, matrix, fw);
+    fclose(fw);
+  }
+}
+
+/**
+ * Matrix printer
+ * For debugging
+ * Kind of a little ASCII art
+ */
+void matrix_printer(int nodes, uint8_t *matrix, FILE *output_stream) {
+  fprintf(output_stream, "   |");
+	for (int i = 0; i < nodes; i++) {
+		fprintf(output_stream, "% d |", i);
+	}
+  fprintf(output_stream, "\n");
+	for (int i = 0; i < nodes; i++) {
+		for (int j = 0; j <= nodes; j++) {
+			fprintf(output_stream, "----");
+		}
+		fprintf(output_stream, "\n %d |", i);
+		for (int j = 0; j < nodes; j++) {
+			fprintf(output_stream, " %d |", matrix[i * nodes + j]);
+		}
+    fprintf(output_stream, "\n");
+	}
+}
---
+++ main.c
@@ -0,0 +1,32 @@
+/**
+ *
+ * File: main.c
+ * Description: Main file for the graph generator
+ * Author: Marek Jankech <xjanke01@stud.fit.vutbr.cz>
+ *
+ **/
+
+#include "generator.h"
+
+int main() {
+  int n = 5, e = 8;  //Specifying the number of nodes and edges that would be processed
+	uint8_t *matrix;
+	
+	//Seed the generator of random numbers
+	srand(time(NULL));
+  //Allocate memory for adjacency matrix
+	if((matrix = (uint8_t *) malloc(n * n * sizeof(uint8_t))) == NULL) {
+		perror("malloc");
+		exit(1);
+	}
+  
+	//Create random graph with selected property and parameters
+	graph_generating_manager(matrix, n, e, CONNECTED);
+	//Print the matrix - for debugging
+  matrix_printing_manager(n, matrix, false);
+  //Free the memory for the matrix
+  free(matrix);
+  
+  return 0;
+}
+
