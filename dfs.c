a77ce6843c5daffa0786c3873f2d7f80f357e03d
David Benes <benys94@gmail.com>
2016-11-28 01:01:09 +0100

    * DFS improved for semi-eulerian graph.
    DFS algorithm is now able to find every path in semi-eulerian graph.

--- dfs.c
+++ dfs.c
@@ -10,30 +10,27 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include "dfs.h"
 #include "error.h"
 #include "stack.h"
 #include "pool.h"
 
-#define MAX_DEPTH (8)
+// Method prints stack content into log file
+void stack_dump(PWrap *wrapper)
+{
+    FILE *logFile = fopen("log.txt", "w");
+    fprintf(logFile, "[\n");
+    for(PWrap *tmpWrap = wrapper; tmpWrap -> next != NULL; tmpWrap = tmpWrap -> next){
+        fprintf(logFile, "\t[\n");
+        for(SWay *tmpWay = tmpWrap -> path; tmpWay -> next != NULL; tmpWay = tmpWay -> next){
+            fprintf(logFile, "\t\t[%d, %d]\n", tmpWay -> coordinates[0], tmpWay -> coordinates[1]);
+        }
+        fprintf(logFile, "\t],\n");
+    }
+    fprintf(logFile, "]\n");
 
-int operations[16][2] = {
-    {1,2},
-    {1,3},
-    {1,4},
-    {2,1},
-    {2,3},
-    {2,4},
-    {3,1},
-    {3,2},
-    {3,4},
-    {3,5},
-    {4,1},
-    {4,2},
-    {4,3},
-    {4,5},
-    {5,3},
-    {5,4},
-};
+    fclose(logFile);
+}
 
 void print_way(SWay *sOpen)
 {
@@ -47,60 +44,61 @@ void print_way(SWay *sOpen)
     printf("\n");
 }
 
-bool dfs_run(PWrap *wrapper, int actNode[], int depth)
+void pathSeeker(PWrap *wrapper, GParams *params, unsigned int actNode, size_t actDepth)
 {
     if(wrapper_empty(wrapper)){
-        if(depth == 0){
-            FatalError(EC_BAD_PATH, "No origin node.");
-        }
+        FatalError(EC_BAD_PATH, "No origin node.");
     }
 
     SWay *sOpen = top_stack(wrapper);
     wrapper = pop_stack(wrapper);
 
 
-    if(depth == MAX_DEPTH){
-        if(sOpen -> coordinates[1] == 1){
+    if(actDepth == params -> depth){
+        if(sOpen -> coordinates[1]){
             print_way(sOpen);
-            return true;
+            pathSeeker(wrapper, params, STop(top_stack(wrapper)), SLen(wrapper -> path));
+            return ;
         }
         else {
             FatalError(EC_BAD_PATH, "Founded path is not eulerian.");
         }
     }
 
-    for(int i = 0; i < 16; i++){
-        if(actNode[1] == operations[i][0]){
-            if( ! in_stack(sOpen, operations[i][0], operations[i][1])){
-                SWay *new_stack = SPush(sOpen, operations[i][0], operations[i][1]);
+    bool deadEnd = true;
+    for(size_t i = 0; i < params -> opsLen; i++){
+        if(actNode == params -> ops[i][0]){
+            if( ! in_stack(sOpen, params -> ops[i][0], params -> ops[i][1])){
+                SWay *new_stack = SPush(sOpen, params -> ops[i][0], params -> ops[i][1]);
                 wrapper = push_stack(wrapper, new_stack);
+                deadEnd = false;
             }
         }
     }
 
-    if( ! dfs_run(wrapper, STop(top_stack(wrapper)), ++depth)){
-        return false;
+    if(deadEnd){
+        if(wrapper -> path != NULL){
+            pathSeeker(wrapper, params, STop(top_stack(wrapper)), SLen(wrapper -> path));
+        }
+        return ;
     }
 
-    return true;
+    pathSeeker(wrapper, params, STop(top_stack(wrapper)), ++actDepth);
 }
 
-int main()
+void warmUp(GParams *details)
 {
-    pool_init();
 
-    PWrap *wrapper = wrap_init();
+    if(details -> semiEulerian){
+        for(int start = 0; start < 2; start++){
 
-    int poc[2] = {0, 2};
-    SWay *origin = SInit();
+            PWrap *wrapper = wrap_init();
+            SWay *origin = SInit();
 
-    SWay *new_stack = SPush(origin, poc[0], poc[1]);
-    wrapper = push_stack(wrapper, new_stack);
-
-    if( ! dfs_run(wrapper , poc, 0)){
-        return EXIT_FAILURE;
+            SWay *new_stack = SPush(origin, 0, details -> semiOrigins[start]);
+            wrapper = push_stack(wrapper, new_stack);
+            pathSeeker(wrapper, details, details -> semiOrigins[start], 0);
+        }
     }
 
-    pool_free();
-    return EXIT_SUCCESS;
 }
